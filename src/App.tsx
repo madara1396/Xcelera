ea98cc73467e18ff01617db739588d5bd6d24aaf
Madara <ggghhhhhhhhhhhhhhhh023@gmail.com>
2025-05-18 22:12:47 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
           <div className="text-center mb-8">
             <div className="w-32 h-32 mx-auto mb-4">
               <img 
-                src="https://raw.githubusercontent.com/stackblitz/stackblitz-icons-source/main/public/xcerela.png" 
+                src="logo.png" 
                 alt="Xcelera Logo" 
                 className="w-full h-full object-contain"
               />
